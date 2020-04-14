9b8c5894b528817cffcf30b23df87160d7a69411
Alex <kavanokami@gmail.com>
2020-04-14 20:42:52 +0400

    feat: return user from protected route

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,21 @@ const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 
 const User = require('../models/User');
 
 //@route    GET api/auth
 //@desc     Get logged in user
 //@access   Private
-router.get('/', (req, res) => {
-  res.send('get logged in user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
 });
 
 //@route    POST api/auth
