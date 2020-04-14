a3deafb81895f1acf3fb80711be29f1e4ed55e9d
Alex <kavanokami@gmail.com>
2020-04-14 17:35:00 +0400

    feat: create and save user to DB

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
 
@@ -14,12 +15,35 @@ router.post(
     check('email', 'not a valid email').isEmail(),
     check('password', 'min 3 chars').isLength({ min: 3 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('passed');
+
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).send({ msg: 'User already exists' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt();
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('user saved');
+    } catch (error) {
+      res.status(500).send('Server Error');
+    }
   }
 );
 
