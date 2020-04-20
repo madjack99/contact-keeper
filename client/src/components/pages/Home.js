3779906a57542015809a29aa6538fea3cb775879
Alex <kavanokami@gmail.com>
2020-04-20 19:25:41 +0400

    feat: add addContact reducer

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import Contacts from '../contacts/Contacts';
+import ContactForm from '../contacts/ContactForm';
 
 const Home = () => {
   return (
-    <div class='grid-2'>
-      <div>{/* contact form */}</div>
+    <div className='grid-2'>
+      <div>
+        <ContactForm />
+      </div>
       <div>
         <Contacts />
       </div>
