ab1ed9ab1736c0b8e77b5b6436d6f997c35c6648
Alex <kavanokami@gmail.com>
2020-04-20 18:13:09 +0400

    feat: add ContactItem

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Contacts from '../contacts/Contacts';
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div class='grid-2'>
+      <div>{/* contact form */}</div>
+      <div>
+        <Contacts />
+      </div>
     </div>
   );
 };
