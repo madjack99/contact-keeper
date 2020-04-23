32efded556595a89bae1830e01237b35aa36d276
Alex <kavanokami@gmail.com>
2020-04-23 16:08:44 +0400

    feat: add contact to DB throut app

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -10,43 +10,34 @@ import {
   UPDATE_CONTACT,
   FILTER_CONTACTS,
   CLEAR_FILTER,
+  CONTACT_ERROR,
 } from '../types';
 
 const ContactState = (props) => {
   const initialState = {
-    contacts: [
-      {
-        id: 1,
-        type: 'professional',
-        name: 'Sarah Smith',
-        email: 'ssmith@gmail.com',
-        phone: '111-111-1111',
-      },
-      {
-        id: 2,
-        type: 'personal',
-        name: 'Ted Johnson',
-        email: 'ted@gmail.com',
-        phone: '222-222-2222',
-      },
-      {
-        id: 3,
-        type: 'personal',
-        name: 'Melissa Williams',
-        email: 'melissa@gmail.com',
-        phone: '777-777-7747',
-      },
-    ],
+    contacts: [],
     current: null,
     filtered: null,
+    error: null,
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add item
-  const addContact = (contact) => {
-    contact.id = uuidv4();
-    dispatch({ type: ADD_CONTACT, payload: contact });
+  const addContact = async (contact) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.post('/api/contacts', contact, config);
+
+      dispatch({ type: ADD_CONTACT, payload: res.data });
+    } catch (error) {
+      dispatch({ type: CONTACT_ERROR, payload: error.response.data.msg });
+    }
   };
 
   // Delete contact
@@ -89,6 +80,7 @@ const ContactState = (props) => {
         updateContact,
         filterContacts,
         clearFilter,
+        error: state.error,
         filtered: state.filtered,
         current: state.current,
         contacts: state.contacts,
